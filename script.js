db50686b85c7affdeb7cbcb27852727a24973ca9
75396 <akosnik1@stu.vistula.edu.pl>
2026-06-07 18:14:40 +0200

    Dodanie Klucza i Url do bazy danych w Supabase

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "";
-const SUPABASE_KEY = "";
+const SUPABASE_URL = "https://mdsgflidcatnbqznrzvc.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kc2dmbGlkY2F0bmJxem5yenZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MzgzOTcsImV4cCI6MjA5NjQxNDM5N30.VmsuMtctCyC79TcdbCTQT3kt6HgZ-O23GzM3OKsGzqg";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
